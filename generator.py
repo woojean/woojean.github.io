782e0f65bd0b6733c17d95649ac281ba09522d51
woojean <168056828@qq.com>
2017-02-25 13:05:17 +0800

    p

--- generator.py
+++ generator.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
         for file in os.listdir(ca[2]):
           filePath= ca[2]+'/'+file
-          contents += '[' + file.replace('.md','') + '](' + filePath +') \n '
+          contents += '[' + file.replace('.md','') + '](' + filePath +') / \n '
 
         contents += '\n\n'
 
